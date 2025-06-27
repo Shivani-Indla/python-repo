b9652cd896f0e1666b1e471e8fc5f275115ff52a
Shivani-Indla <shiva.indla1518@gmail.com>
2025-06-27 11:11:48 +0530

    my commit for python

---
+++ 02-conditions.py
@@ -0,0 +1,7 @@
+user_input = int(input("Enter a number"))
+#user_input = int(user_input)
+
+if user_input > 10:
+    print("given num is graterthan 10")
+else:
+    print("given num is not lessthan or equal to 10")
