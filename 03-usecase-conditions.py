d5bc44bc07d1f7eecfc0211d0d61516aecb26f37
Shivani-Indla <shiva.indla1518@gmail.com>
2025-06-27 12:23:21 +0530

    my commit for python

---
+++ 03-usecase-conditions.py
@@ -0,0 +1,20 @@
+username = "ec2-user"
+password = "Devops123"
+
+in_username = input("Please enter username")
+in_password = input("Please enter password")
+
+if username == in_username:
+    if password == in_password:
+        print("login successfull")
+    else:
+        print("Please enter correct password")
+else:
+    print("Please check user name")
+
+    #or
+
+if (username == in_username) and (password == in_password):
+    print("Login successfull")
+else:
+    print("Please check uname and passwd")
