2c3c937b84cc982c83bf2a112795116325a471dd
Shivani-Indla <shiva.indla1518@gmail.com>
2025-06-29 11:11:14 +0530

    my commit for python

--- 05-functions.py
+++ 05-functions.py
@@ -5,9 +5,13 @@
 
 #Use cases
 
-def add(a, b):
-    res = a + b
+def add(a, b, c=3):
+    """
+        this function is adding a,b,c. "we can get this info in help(add)"
+    """
+    res = a + b + c
     return res
 #calling the function
 result = add(a=1, b=2)
 print(result)
+help(add)
