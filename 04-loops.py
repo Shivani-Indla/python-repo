64bf2d1f04e2ceb852e64264b0fa9e270102cfd8
Shivani-Indla <shiva.indla1518@gmail.com>
2025-06-29 09:49:04 +0530

    my commit for python

--- 04-loops.py
+++ 04-loops.py
@@ -2,3 +2,9 @@ sample_list = ["Ansible", "Devops", "Terraform", "Dcoker"]
 
 for i in sample_list:
     print(i)
+
+    # by using dictionary in loops
+
+sample_dict = {"a: 1", "b: 2", "c= 3"}
+for i,j in sample_dict:
+    print(i,j)
