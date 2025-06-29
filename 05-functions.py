8b25713c4bad5635ab8c849033eee3a8c304ae73
Shivani-Indla <shiva.indla1518@gmail.com>
2025-06-29 10:13:50 +0530

    my commit for python

--- 05-functions.py
+++ 05-functions.py
@@ -1,4 +1,13 @@
-def sample_func():
-    print("This is a sample function")
-# calling this function as bellow
-sample_func()
+# def sample_func():
+#     print("This is a sample function")
+# # calling this function as bellow
+# sample_func()
+
+#Use cases
+
+def add(num_1, num_2):
+    res = num_1 + num_2
+    return res
+#calling the function
+res = add(num_1=1, num_2=2)
+print(res)
